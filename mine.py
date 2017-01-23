ddcfbfa49773a0e03bb3c8d8b7f22f67d6ca2d9e
Alexander Ortiz <aorti017@ucr.ed>
2017-01-23 11:35:07 +0000

    outputs jsno now

--- mine.py
+++ mine.py
@@ -1,27 +1,52 @@
-import spotipy
+import spotipy, json, pprint, sys
 
 sp = spotipy.Spotify()
+pp = pprint.PrettyPrinter(indent=4)
 
-results = sp.search(q='artist:weezer', type='artist')
+query = "weezer"
 
+results = sp.search(q='artist:'+query, type='artist')
 uri = results['artists']['items'][0]['uri']
-#print results['artists']['items'][0]['genres']
-print results['artists']['items'][0]
+
+name = results['artists']['items'][0]['name']
+if name.lower() != query.lower():
+	print "Error"
+	sys.exit()
+
+artist_dict = {}
+artist_dict[name] = {}
+artist_dict[name]['albums'] = []
+
+artist_dict[name]['genres'] = results['artists']['items'][0]['genres']
+artist_dict[name]['followers'] = results['artists']['items'][0]['followers']['total']
+artist_dict[name]['popularity'] = results['artists']['items'][0]['popularity']
+
 
 results = sp.artist_albums(uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = sp.next(results)
-    albums.extend(results['items'])
 
-f = open("test", 'w')
+#need to remove duplicates?
+album_uris = []
+for i in results['items']:
+	album_uris.append(i['uri'])
 
-for album in albums:
-    uri = album['uri']
-    f.write(uri)
-    results = sp.album_tracks(uri)
-    for i in results['items']:
-	    f.write(i['name'].encode('utf-8'))
-    f.write("---------------")
-f.close()
+results = sp.albums(album_uris)
+
+track_uris = []
+for i in results['albums']:
+	artist_dict[name]['albums'].append({})
+	artist_dict[name]['albums'][-1]['total_tracks'] = i['tracks']['total']
+	artist_dict[name]['albums'][-1]['name'] = i['name']
+ 	artist_dict[name]['albums'][-1]['uri'] = i['uri']
+	artist_dict[name]['albums'][-1]['popularity'] = i['popularity']
+	artist_dict[name]['albums'][-1]['release_date'] = i['release_date']
+	artist_dict[name]['albums'][-1]['tracks'] = []
+	for j in i['tracks']['items']:
+		artist_dict[name]['albums'][-1]['tracks'].append({})
+		artist_dict[name]['albums'][-1]['tracks'][-1]['duration'] = j['duration_ms']
+		artist_dict[name]['albums'][-1]['tracks'][-1]['name'] = j['name']
+		artist_dict[name]['albums'][-1]['tracks'][-1]['uri'] = j['uri']
+
+print json.dumps(artist_dict, ensure_ascii=True)
+#pp.pprint(artist_dict)
+
 
