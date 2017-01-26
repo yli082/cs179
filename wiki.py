17b5f99fb34bf0d9402c1e0eaba34b7f997e6542
Dustin Pham <dpham008@ucr.edu>
2017-01-26 09:10:29 -0800

    modify what wiki.py pulls and new.py combines wiki and mine

--- wiki.py
+++ wiki.py
@@ -9,31 +9,17 @@ for i in w.links:
 		out = {}
 		out[j.encode('utf-8')] = ''
 		try:
-			a = wikipedia.WikipediaPage(title=j)
-			html = a.html()
-			start = html.find('<th scope="row">Born</th>')
-			if start == -1:
-				start = html.find('<th scope="row">Origin</th>')
-			if start == -1:
-				pass
-			else:
-				end = html[start:].find('</td>')
-				end += start
-				chunk = html[start:end]
-				origin = ""
-				while chunk.find('<a ') != -1:
-					fake_start = chunk.find('title="')
-					start = chunk[fake_start+7:].find('>')
-					start += 7
-					end = chunk[fake_start+2:].find('<')
-					start += fake_start+1
-					end += fake_start+2
-					if origin == "":
-						origin += chunk[start: end].encode('utf-8')
-					else:
-						origin = origin + ' ' + chunk[start: end].encode('utf-8')
-					chunk = chunk[end+1:]
-				out[j.encode('utf-8')]= origin
+			a = wikipedia.WikipediaPage(title="weezer")
+			summary = a.summary
+			content = a.content
+			ref = a.references
+			out[j.encode('utf-8')] = {}
+			out[j.encode('utf-8')]["summary"] = summary 
+			out[j.encode('utf-8')]["content"] = content
+			out[j.encode('utf-8')]["references"] = ref
+			
 		except:
 			pass
 		print out
+		print "-----------------------------------------------"
+		sys.exit()
